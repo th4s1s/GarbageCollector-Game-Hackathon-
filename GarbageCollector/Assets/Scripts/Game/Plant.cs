265c9d5f8dd3375847e04ff5fc96e6196eea3031
tung.doanduy <sion080803@gmail.com>
2022-11-26 19:38:19 +0700

    a

--- GarbageCollector/Assets/Scripts/Game/Plant.cs
+++ GarbageCollector/Assets/Scripts/Game/Plant.cs
@@ -7,6 +7,7 @@ public class Plant : MonoBehaviour
     [SerializeField] float timeToGrow;
     [SerializeField] float powerScale;
     public bool isMature = false;
+    public bool isDead = false;
     float timePlant;
     float timeSpawnOxy=0;
     GameObject plant;
@@ -26,32 +27,38 @@ public class Plant : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log(isMature);
-        if (Time.time - timePlant<=timeToGrow+0.2f)
+        if (!isDead)
         {
-            if (Time.time - timePlant >= timeToGrow / 2 && Time.time - timePlant < timeToGrow)
+            if (Time.time - timePlant <= timeToGrow + 0.2f)
             {
-                float timeVar = Time.time - timePlant - timeToGrow / 2;
-                float scaleFunction = (-100 * powerScale + 100) * timeVar * timeVar + (20 * powerScale - 20) * timeVar + 1;
-                if (timeVar <= 0.2f) size.localScale = new Vector3(scaleFunction, scaleFunction, 1);
-                if (timeVar>=0.1f) anim.Play("Stage_2");
+                if (Time.time - timePlant >= timeToGrow / 2 && Time.time - timePlant < timeToGrow)
+                {
+                    float timeVar = Time.time - timePlant - timeToGrow / 2;
+                    float scaleFunction = (-100 * powerScale + 100) * timeVar * timeVar + (20 * powerScale - 20) * timeVar + 1;
+                    if (timeVar <= 0.2f) size.localScale = new Vector3(scaleFunction, scaleFunction, 1);
+                    if (timeVar >= 0.1f) anim.Play("Stage_2");
+                }
+                if (Time.time - timePlant >= timeToGrow)
+                {
+                    float timeVar = Time.time - timePlant - timeToGrow;
+                    float scaleFunction = (-100 * powerScale + 100) * timeVar * timeVar + (20 * powerScale - 20) * timeVar + 1;
+                    if (timeVar <= 0.2f) size.localScale = new Vector3(scaleFunction, scaleFunction, 1);
+                    if (timeVar >= 0.1f) anim.Play("Stage_3");
+                    isMature = true;
+                }
             }
-            if (Time.time - timePlant >= timeToGrow)
+            if (isMature)
             {
-                float timeVar = Time.time - timePlant - timeToGrow;
-                float scaleFunction = (-100 * powerScale +100 ) * timeVar * timeVar + (20 * powerScale - 20) * timeVar + 1;
-                if (timeVar <= 0.2f) size.localScale = new Vector3(scaleFunction, scaleFunction, 1);
-                if (timeVar >=0.1f) anim.Play("Stage_3");
-                isMature = true;
+                if (Time.time >= timeSpawnOxy + 10)
+                {
+                    timeSpawnOxy = Time.time;
+                    GameController.Instance.Oxy += oxy;
+                }
             }
         }
-        if (isMature)
+        if (isDead)
         {
-            if (Time.time>=timeSpawnOxy+10)
-            {
-                timeSpawnOxy = Time.time;
-                GameController.Instance.Oxy += oxy;
-            }
+            anim.Play("Dead");
         }
     }
     
