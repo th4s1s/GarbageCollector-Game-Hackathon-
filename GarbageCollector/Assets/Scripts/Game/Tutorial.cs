30489fd5577b41d62f3aff12e6fe8e55f175168d
haiphan239 <97886279+haiphan239@users.noreply.github.com>
2022-11-27 02:10:08 +0700

    ,

--- GarbageCollector/Assets/Scripts/Game/Tutorial.cs
+++ GarbageCollector/Assets/Scripts/Game/Tutorial.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Tutorial : MonoBehaviour
 {
     int tutorialIndex = 0;
-    [SerializeField] Animator tutorialAnim;
+    [SerializeField] List<GameObject> listPanel = new List<GameObject>();
+    [SerializeField] GameObject machineObj;
+    [SerializeField] GameObject blackScene;
 
+    private void Start()
+    {
+        blackScene.SetActive(true);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -18,7 +25,22 @@ public class Tutorial : MonoBehaviour
 
     void NextTutorial()
     {
-        string st = "Tutorial" + tutorialIndex.ToString();
-        tutorialAnim.Play(st);
+        tutorialIndex++;
+        if (tutorialIndex>=listPanel.Count)
+        {
+            listPanel[tutorialIndex - 1].SetActive(false);
+            blackScene.GetComponent<Animator>().Play("BlackSceneEnd");
+            Invoke("NextScene", 2);
+        }
+        if (tutorialIndex == 5 || tutorialIndex == 6) machineObj.SetActive(true);
+        else machineObj.SetActive(false);
+
+        listPanel[tutorialIndex].SetActive(true);
+        listPanel[tutorialIndex - 1].SetActive(false);
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
