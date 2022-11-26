16570613b1323e54e2e24884ceac70a66d833344
bentnt1982 <bentnt1982@gmail.com>
2022-11-26 12:50:12 +0700

    Garbage Spawner

---
+++ Hackathon/Assets/Scripts/Garbage/GarbageSpawner.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GarbageSpawner : MonoBehaviour
+{
+    [SerializeField] float spawnTime;
+    [SerializeField] Vector2 spawnZoneX;
+    [SerializeField] Vector2 spawnZoneY;
+    [SerializeField] GarbageSpawnerData spawnerData;
+    [SerializeField] Garbage garbageTemplate;
+    [SerializeField] Slider pollutionMeter;
+    [SerializeField] float maxPollution;
+    public static GarbageSpawner Instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        pollutionMeter.value = 0;
+        StartCoroutine(GarbageWave());
+    }
+
+    void SpawnGarbage()
+    {
+        int idx = Random.Range(0, spawnerData.Garbages.Count);
+        GameObject garbageInstance = Instantiate(garbageTemplate.gameObject);
+        garbageInstance.transform.position = new Vector2(Random.Range(spawnZoneX.x, spawnZoneX.y), Random.Range(spawnZoneY.x, spawnZoneY.y));
+        garbageInstance.GetComponent<Garbage>().Setup(spawnerData.Garbages[idx].ID, spawnerData.Garbages[idx].Icon, spawnerData.Garbages[idx].Price, spawnerData.Garbages[idx].Type, spawnerData.Garbages[idx].PollutionAmount);
+        ChangePollutionMeter(spawnerData.Garbages[idx].PollutionAmount);
+    }
+
+    IEnumerator GarbageWave()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            SpawnGarbage();
+        }
+    }
+
+    void ChangePollutionMeter(int amount)
+    {
+        float val = pollutionMeter.value + ((float)amount / maxPollution * 1.0f);
+        if (val < 0) val = 0;
+        pollutionMeter.value = val;
+    }
+
+}
